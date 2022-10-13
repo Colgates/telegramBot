4927ba0c54ad6b0a0571fa6481564a016b22a428
Evgenii Kolgin <mista.colgate@gmail.com>
2022-10-13 14:09:15 +0800

    configure uncomment

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -6,15 +6,15 @@ public func configure(_ app: Application) throws {
     // uncomment to serve files from /Public folder
     // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
 
-//    let tgApi: String = Environment.get("BOT_TOKEN")!
-//    let connection: TGConnectionPrtcl = TGLongPollingConnection()
-//    TGBot.configure(connection: connection, botId: tgApi, vaporClient: app.client)
-//    try TGBot.shared.start()
+    let tgApi: String = Environment.get("BOT_TOKEN")!
+    let connection: TGConnectionPrtcl = TGLongPollingConnection()
+    TGBot.configure(connection: connection, botId: tgApi, vaporClient: app.client)
+    try TGBot.shared.start()
 
     /// set level of debug if you needed
-//    TGBot.log.logLevel = .error
-
-//    DefaultBotHandlers.addHandlers(app: app, bot: TGBot.shared)
+    TGBot.log.logLevel = .error
+    DefaultBotHandlers.addHandlers(app: app, bot: TGBot.shared)
+    
     // register routes
     try routes(app)
 }
